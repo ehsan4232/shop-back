91324d206cda4fa0df48060c32ec50218b7a23e4
ehsan4232 <ehsan.samiei4232@gmail.com>
2025-07-28 03:15:50 +0330

    Add Django URLs and Celery configuration

---
+++ mall/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+api_patterns = [
+    path('auth/', include('apps.accounts.urls')),
+    path('stores/', include('apps.stores.urls')),
+    path('products/', include('apps.products.urls')),
+    path('orders/', include('apps.orders.urls')),
+]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include(api_patterns)),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
