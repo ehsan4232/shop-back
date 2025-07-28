cfc9ccf11436036e9b46c990a7d6231b9a855f39
ehsan4232 <ehsan.samiei4232@gmail.com>
2025-07-29 00:55:13 +0330

    Add communications admin configuration

--- apps/communications/admin.py
+++ apps/communications/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(SMSTemplate)
+class SMSTemplateAdmin(admin.ModelAdmin):
+    list_display = ['store', 'name', 'template_type', 'is_active', 'usage_count']
+    list_filter = ['template_type', 'is_active']
+    search_fields = ['name', 'store__name_fa']
+
+@admin.register(SMSMessage)
+class SMSMessageAdmin(admin.ModelAdmin):
+    list_display = ['recipient', 'store', 'status', 'cost', 'created_at']
+    list_filter = ['status', 'created_at']
+    search_fields = ['recipient', 'content']
+    readonly_fields = ['created_at', 'sent_at', 'delivered_at']
+
+@admin.register(EmailTemplate)
+class EmailTemplateAdmin(admin.ModelAdmin):
+    list_display = ['store', 'name', 'template_type', 'is_active', 'usage_count']
+    list_filter = ['template_type', 'is_active']
+    search_fields = ['name', 'subject', 'store__name_fa']
+
+@admin.register(PushNotification)
+class PushNotificationAdmin(admin.ModelAdmin):
+    list_display = ['store', 'title', 'target_type', 'status', 'sent_count', 'scheduled_at']
+    list_filter = ['target_type', 'status']
+    search_fields = ['title', 'body']
+    readonly_fields = ['sent_count', 'created_at', 'sent_at']
