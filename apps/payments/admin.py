9fc0b4718376de72d001305f5115d85d09ff32be
ehsan4232 <ehsan.samiei4232@gmail.com>
2025-07-29 01:16:59 +0330

    Add payments admin configuration

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import PaymentGateway, Payment, Refund
+from .models import *
 
 @admin.register(PaymentGateway)
 class PaymentGatewayAdmin(admin.ModelAdmin):
@@ -7,12 +7,6 @@ class PaymentGatewayAdmin(admin.ModelAdmin):
     list_filter = ['gateway', 'is_active', 'is_sandbox']
     search_fields = ['store__name_fa', 'merchant_id']
     readonly_fields = ['created_at', 'updated_at']
-    
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        if request.user.is_superuser:
-            return qs
-        return qs.filter(store__owner=request.user)
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
@@ -20,16 +14,11 @@ class PaymentAdmin(admin.ModelAdmin):
     list_filter = ['status', 'gateway__gateway', 'created_at']
     search_fields = ['order__order_number', 'transaction_id', 'reference_id']
     readonly_fields = ['created_at', 'paid_at', 'verified_at']
-    
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        if request.user.is_superuser:
-            return qs
-        return qs.filter(order__store__owner=request.user)
+    date_hierarchy = 'created_at'
 
 @admin.register(Refund)
 class RefundAdmin(admin.ModelAdmin):
-    list_display = ['payment', 'amount', 'status', 'reason', 'created_at']
+    list_display = ['payment', 'amount', 'status', 'created_at', 'processed_at']
     list_filter = ['status', 'created_at']
     search_fields = ['payment__order__order_number', 'refund_id']
     readonly_fields = ['created_at', 'processed_at']
