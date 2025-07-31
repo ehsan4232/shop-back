a72300a9ea061c576a5faec30e62cd2736ea287a
ehsan4232 <ehsan.samiei4232@gmail.com>
2025-08-01 02:35:46 +0330

    Delete apps/themes directory

--- apps/themes/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ThemesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.themes'
-    verbose_name = 'قالب‌ها و شخصی‌سازی'
